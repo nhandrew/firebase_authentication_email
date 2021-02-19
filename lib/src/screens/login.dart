54c331441952c643f1274d7a15ae700bd1330d4c
nhandrew <fourjulows@yahoo.com>
2021-02-19 17:11:34 -0500

    Add Error trapping

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:login_app/src/screens/home.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,18 +14,18 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
-      appBar: AppBar(title: Text('Login'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(  
-                hintText: 'Email'
-              ),
-               onChanged: (value) {
+              decoration: InputDecoration(hintText: 'Email'),
+              onChanged: (value) {
                 setState(() {
                   _email = value.trim();
                 });
@@ -42,32 +43,51 @@ class _LoginScreenState extends State<LoginScreen> {
                 });
               },
             ),
-            
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children:[
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Signin'),
+                onPressed: () => _signin(_email, _password)
+            ),
             RaisedButton(
               color: Theme.of(context).accentColor,
-              child: Text('Signin'),
-              onPressed: (){
-                  auth.signInWithEmailAndPassword(email: _email, password: _password).then((_){
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
-                  });
-                  
-            }),
-            RaisedButton( 
-              color: Theme.of(context).accentColor,
               child: Text('Signup'),
-              onPressed: (){
-                auth.createUserWithEmailAndPassword(email: _email, password: _password).then((_){
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
-                });
-                
-              },
+              onPressed: () => _signup(_email, _password),
             )
           ])
-        ],),
+        ],
+      ),
     );
+  }
+
+  _signin(String _email, String _password) async {
+    try {
+      //Create Get Firebase Auth User
+      await auth.signInWithEmailAndPassword(email: _email, password : _password);
+
+      //Success
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomeScreen()));
+
+    } on FirebaseAuthException catch (error) {
+      Fluttertoast.showToast(msg: error.message,gravity: ToastGravity.TOP);
+    }
+
+  }
+
+  _signup(String _email, String _password) async {
+    try {
+      //Create Get Firebase Auth User
+      await auth.createUserWithEmailAndPassword(email: _email, password : _password);
+
+      //Success
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomeScreen()));
+
+    } on FirebaseAuthException catch (error) {
+      Fluttertoast.showToast(msg: error.message,gravity: ToastGravity.TOP,);
+    }
+
   }
 }
